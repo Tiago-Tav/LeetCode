117024db38cb3a8880cce84f0b5d05216de0e56e
Tiago Tavares <83955268+Tiago-Tav@users.noreply.github.com>
2025-03-11 15:50:36 -0300

    Delete LC1 two sum directory

--- LC1 two sum/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        numMap = {}
-        for n in range(len(nums)):
-            if (target - nums[n]) in numMap:
-                return [numMap[target - nums[n]], n]
-            numMap[nums[n]] = n

13c82dd8e70a3aad96fa0a1dc2e1213759ea81e1
Tiago Tavares <83955268+Tiago-Tav@users.noreply.github.com>
2025-03-15 16:03:41 -0300

    Added 15/03/2025

--- 20-valid-parenthses/solution.py
+++ 20-valid-parenthses/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        opened = []
-        for c in s:
-            if c == '{' or c == '(' or c == '[':
-                opened.append(c)
-            else:
-                if opened and (opened[-1] == chr(ord(c) - 1) or opened[-1] == chr(ord(c) - 2)):
-                    opened.pop()
+        equivalent = {
+            ')' : '(',
+            ']' : '[',
+            '}' : '{'
+        }
+        stack = []
+        for char in s:
+            if char in equivalent:
+                if not stack:
+                    return False
+                if equivalent[char] == stack[-1]:
+                    stack.pop()
                 else:
                     return False
-        if opened == []:
-            return True
-        else:
-            return False
+            else:
+                stack.append(char)
+        return not stack
